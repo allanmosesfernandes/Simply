96e56ca8812e088990911f8931250d3a597dba25
Allan Fernandes <Fernzallan14@gmail.com>
2023-08-11 10:47:38 +0100

    found a proxy

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 /* Fetching API response */
-const API_URL = "https://frontendtest.pleasecheck.me/wp-json/wp/v2/posts/";
+const API_URL = "https://corsproxy.io/?https://frontendtest.pleasecheck.me/wp-json/wp/v2/posts/";
 
 export async function fetchQuotes() {
   try {
@@ -8,17 +8,7 @@ export async function fetchQuotes() {
     return responseJson.map((quote) => quote.title.rendered);
   } catch (error) {
     console.log("The API woudln't work so I came up with a bandage solution (: ", error.message);
-    return [
-      "Prime number the carbon in our apple pies extraord…io telescope Apollonius of Perga radio telescope.",
-      "Tendrils of gossamer clouds ship of the imaginatio… Paroxysm of global death permanence of the stars",
-      "Cosmos tendrils of gossamer clouds dispassionate e…he universe a mote of dust suspended in a sunbeam",
-      "Tingling of the spine light years vanquish the imp…s intelligent beings tendrils of gossamer clouds.",
-      "Tingling of the spine dream of the mind&#8217;s ey…a Sea of Tranquility white dwarf rings of Uranus.",
-      "￼￼Muse about a very small stage in a vast cosmic",
-      "￼Brain is the seed of intelligence concept of the number",
-      "￼Another article title brought over from the blog",
-      "Flatland worldlets Apollonius of Perga hydrogen atoms extraordina",
-    ];
+    return ["I'm sorry, the API is not working right now. Please try again later."];
   }
 
 }
